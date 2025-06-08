928f6fa1ec3cf2a47bf07f6b2cae16055dafe6a1
tttngan0412 <412ngan@gmail.com>
2025-06-08 19:00:46 +0700

    first commit

---
+++ TC01.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Đường dẫn Edge WebDriver
+EDGE_DRIVER_PATH = "D:\\Ngan nam 4\\4-Fourth-year\\Semester-2\\BookStore\\Test\\msedgedriver.exe"
+
+# Khởi động WebDriver
+service = Service(EDGE_DRIVER_PATH)
+driver = webdriver.Edge(service=service)
+try:
+    # vào trang đăng nhập
+    driver.get("https://localhost:7226/")
+    time.sleep(1)
+
+    # Điền thông tin đăng nhập
+    driver.find_element(By.ID, "email").send_keys("phucnaoto@gmail.com")
+    driver.find_element(By.ID, "password").send_keys("123456")
+    time.sleep(1)
+
+    # click btn login
+    driver.find_element(By.XPATH, "//button[text()='Sign In']").click()
+    time.sleep(5)
+
+    # chọn sách tôi là be tô
+    checkbox = driver.find_element(By.XPATH, "(//input[@type='checkbox'])[3]")  # checkbox thứ 3
+    checkbox.click()
+    time.sleep(1)
+
+    # click button order
+    order_button = driver.find_element(By.XPATH, "//button[span[text()='Order']]")
+    order_button.click()
+    time.sleep(1)
+     # tìm và lấy nhân viên thùy ngân
+    comboboxCustomer = driver.find_element(By.XPATH, "//input[@name='CustomerComboboxId']")
+    comboboxCustomer.send_keys("Thùy Ngân")
+    # Nhấn Enter để chọn
+    comboboxCustomer.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+     # tìm và lấy khuyến mãi mùa hè
+    comboboxPromotion = driver.find_element(By.XPATH, "//input[@name='PromotionComboboxId']")
+    comboboxPromotion.send_keys("Khuyến Mãi Mùa Hè")
+    # Nhấn Enter để chọn
+    comboboxPromotion.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+
+
+    # click button save
+    order_button = driver.find_element(By.XPATH, "//button[span[text()='Save']]")
+    order_button.click()
+
+    # Đợi toast chứa text xuất hiện trong vòng 5 giây
+    WebDriverWait(driver, 5).until(
+        EC.text_to_be_present_in_element(
+            (By.CLASS_NAME, "custom-toast-background"),
+            "Order created successfully."
+        )
+    )
+    print("✅ Toast chứa nội dung mong đợi.")
+    time.sleep(10)
+
+except Exception as e:
+    print(f"❌ Đã xảy ra lỗi: {e}")
+
+finally:
+    driver.quit()
